e858f9b55ca0dc422317c9e6a4c83285c9a8c72e
Karol Żebrowski <19karol98@gmail.com>
2021-03-13 15:40:38 +0100

    Bug fixes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const exchangeRouter = require("./routers/exchange");
-// const port = process.env.PORT;
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
--- src/routers/exchange.js
+++ src/routers/exchange.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
-router.get("/exchange", async (req, res) => {
+const instance = axios.create({
+  baseURL: "http://api.nbp.pl/api/exchangerates/rates",
+});
+
+router.get("/exchange_rate", async (req, res) => {
   try {
-    res.status(200).send("Working!");
+    let rate = 0;
+    instance.get("/a/gbp/?format=json").then((response) => {
+      rate = response.data.rates[0].mid;
+      res.status(200).send({ rate });
+    });
   } catch (error) {
     res.status(400).send(error);
   }
